4ba5f9612990588f296b81b4a440107fe5c90fd5
Ekaterina Marinicheva <marri.nich@yandex.ru>
2021-12-29 13:56:47 +0300

    feat: add code to resolve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
-}
+    let newNumber = Array.from(String(n)).reverse();
+
+    let answer = '';
+    
+    for (let i = 0; i < newNumber.length; i++) {
+        if (Number.isInteger(+newNumber[i]) ) {
+            answer += newNumber[i];
+        }        
+    }
+    console.log(answer);
+    return +answer;
+};
